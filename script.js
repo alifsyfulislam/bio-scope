1b4acefdb9545a71a5a4355ebeee3370ff4ae7f0
alifsyfulislam <alifsyfulislam@gmail.com>
2020-03-25 17:39:18 +0600

    end

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-$('.patch_box').click(()=>{
-    console.log('1111');
+var myChoice = 0;
+
+$('.patch_slider_2').css({display : 'none'});
+
+$('.patch_board,.patch_board_left,.patch_board_right').click(()=>{
+    myChoice++;
     $('.patch_board').toggleClass('animate');
-    $('.patch_slider').css({display : none});
+    if (myChoice%2 != 0){
+        zoomInAll();
+    }
+    else {
+        zoomOutAll();
+    }
+});
+
+function zoomInAll() {
+    $('.patch_slider').css({display : 'none'});
+    $('.patch_board_right').css({display : 'none'});
+    $('.patch_board_left').css({display : 'none'});
+    $('.patch_des').css({display : 'none'});
+    $('.patch_boisak').css({display : 'none'});
+    $('.patch_offer').css({display : 'none'});
+    setTimeout(()=>{
+        $('.patch_slider_2').css({display : 'block'});
+        $('.patch_cta').css({display : 'block'});
+    },1.5e3);
+}
+
+function zoomOutAll() {
+    $('.patch_slider_2').css({display : 'none'});
+    $('.patch_cta').css({display : 'none'});
+    $('.patch_board_right').css({display : 'block'});
+    $('.patch_board_left').css({display : 'block'});
+    $('.patch_des').css({display : 'block'});
+    $('.patch_boisak').css({display : 'block'});
+    $('.patch_offer').css({display : 'block'});
+    setTimeout(()=>{
+        $('.patch_slider').css({display : 'block'});
+    },1.5e3);
+}
+
+$('.patch_cta').click(()=>{
+   window.open('https://purplepatch.online/currents','_blank');
 });
